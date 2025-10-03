ca1cda61bb810a1d7ffeacf379eddfa42751d860
Shubham <shubhamsaini8965@gmail.com>
2025-10-03 14:18:23 +0530

    baisc changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ import PropertyVerification from "../components/PropertyVerification"
 
 const Dashboard = () => {
   return (
-    <div className="h-[650px] overflow-x-hidden overflow-y-scroll">
+    <div className="lg:h-[650px] overflow-x-hidden lg:overflow-y-scroll">
       <div className='mt-5'>
         <h3 className='text-[24px]'>Reports & Analytics</h3>
         <p className='text-[12px] text-[#838383]'>Monitor platform performance, trends, and detailed statistics to improve decision-making.</p>
